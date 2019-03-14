54593d39baaba5fa0f5aac61fcd154d0c76e7e16
Katie Blizzard <kblizzard11@gmail.com>
2019-03-14 16:15:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,16 +12,15 @@ class Dog
   end 
   
   
-  def self.clear_all 
-    @@all.clear   
-  end 
-  
   def self.all 
     @@all.each do |x|
       puts self.name 
     end 
   end 
  
+ def self.clear_all 
+    @@all.clear   
+  end 
   
 
 end 
